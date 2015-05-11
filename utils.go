2a8a668e488567e62d9b6e973eb57ec5df0b4441
Senthil <senthil196@gmail.com>
2015-05-10 18:36:08 -0700

    move random seed to init f(x)

--- utils.go
+++ utils.go
@@ -6,12 +6,14 @@ import (
 	"time"
 )
 
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
 // Taken from: http://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func randSeq(n int) string {
-	rand.Seed(time.Now().Unix())
-
 	b := make([]rune, n)
 	for i := range b {
 		b[i] = letters[rand.Intn(len(letters))]
